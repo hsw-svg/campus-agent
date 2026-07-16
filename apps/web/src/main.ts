53d87b440eb386f99a4870ba9f3df5d78038dd0a
Huangshaowei <13446378+shgfshsrthts@user.noreply.gitee.com>
2026-07-16 09:53:17 +0800

    前端样式更改

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import './styles/inspira-ui.css'
 
 createApp(App).use(createPinia()).mount('#app')
